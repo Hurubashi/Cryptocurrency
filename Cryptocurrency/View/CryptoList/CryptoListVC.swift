5a65c5925c2705a88930d0737992cf8347c9dd00
Max Rybak <hurubashi@gmail.com>
2019-03-01 20:54:10 +0200

    App skeleton created

---
+++ Cryptocurrency/View/CryptoList/CryptoListVC.swift
@@ -0,0 +1,30 @@
+//
+//  CryptoListVC.swift
+//  Cryptocurrency
+//
+//  Created by Max Rybak on 3/1/19.
+//  Copyright © 2019 Max Rybak. All rights reserved.
+//
+
+import UIKit
+
+class CryptoListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
